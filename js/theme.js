b35a94172a2c7b63471d5286e6f1a3a58a345915
Paulo Diovani <paulo@diovani.com>
2017-01-25 16:37:58 -0200

    Add monokai theme for dark bg syntax

--- js/theme.js
+++ js/theme.js
@@ -1,29 +1,25 @@
 document.body.style.backgroundColor = sessionStorage.getItem('bg');
 document.body.style.color = sessionStorage.getItem('cc');
-function theme() {
-     if ( sessionStorage.getItem('bg') === 'rgb(255, 255, 255)') {
-         
-            sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-            sessionStorage.setItem('cc', '#777');
-            
-         
-     }
-    else if (sessionStorage.getItem('bg') == null || undefined) {
-        sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-        sessionStorage.setItem('cc', '#777');
-        
-    }
-    else if( sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
-        
-        sessionStorage.setItem('bg', 'rgb(255, 255, 255)');
-        sessionStorage.setItem('cc', '#333');
-        
-  
-    }
 
-document.body.style.backgroundColor = sessionStorage.getItem('bg');
-document.body.style.color = sessionStorage.getItem('cc');
-
+if (sessionStorage.getItem('syntax')) {
+    document.getElementById('syntax').setAttribute('href', sessionStorage.getItem('syntax'));
 }
 
+function theme() {
+    var syntax = document.getElementById('syntax').href;
 
+    if ( sessionStorage.getItem('bg') === 'rgb(255, 255, 255)' || !sessionStorage.getItem('bg')) {
+        sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
+        sessionStorage.setItem('cc', '#777');
+        sessionStorage.setItem('syntax', syntax.replace('default', 'monokai'));
+    }
+    else if( sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
+        sessionStorage.setItem('bg', 'rgb(255, 255, 255)');
+        sessionStorage.setItem('cc', '#333');
+        sessionStorage.setItem('syntax', syntax.replace('monokai', 'default'));
+    }
+
+    document.body.style.backgroundColor = sessionStorage.getItem('bg');
+    document.body.style.color = sessionStorage.getItem('cc');
+    document.getElementById('syntax').setAttribute('href', sessionStorage.getItem('syntax'));
+}
