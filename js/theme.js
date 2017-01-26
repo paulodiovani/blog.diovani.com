cdc3e0defc547b59c84ce5cdf82c69bab3cc67b9
Paulo Diovani <paulo@diovani.com>
2017-01-26 01:39:41 -0200

    Use localStorage instead of sessionStorage
    ...for saving theme

--- js/theme.js
+++ js/theme.js
@@ -1,25 +1,25 @@
-document.body.style.backgroundColor = sessionStorage.getItem('bg');
-document.body.style.color = sessionStorage.getItem('cc');
+document.body.style.backgroundColor = localStorage.getItem('bg');
+document.body.style.color = localStorage.getItem('cc');
 
-if (sessionStorage.getItem('syntax')) {
-    document.getElementById('syntax').setAttribute('href', sessionStorage.getItem('syntax'));
+if (localStorage.getItem('syntax')) {
+    document.getElementById('syntax').setAttribute('href', localStorage.getItem('syntax'));
 }
 
 function theme() {
     var syntax = document.getElementById('syntax').href;
 
-    if ( sessionStorage.getItem('bg') === 'rgb(255, 255, 255)' || !sessionStorage.getItem('bg')) {
-        sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-        sessionStorage.setItem('cc', '#777');
-        sessionStorage.setItem('syntax', syntax.replace('default', 'monokai'));
+    if ( localStorage.getItem('bg') === 'rgb(255, 255, 255)' || !localStorage.getItem('bg')) {
+        localStorage.setItem('bg', 'rgb(6, 23, 37)');
+        localStorage.setItem('cc', '#777');
+        localStorage.setItem('syntax', syntax.replace('default', 'monokai'));
     }
-    else if( sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
-        sessionStorage.setItem('bg', 'rgb(255, 255, 255)');
-        sessionStorage.setItem('cc', '#333');
-        sessionStorage.setItem('syntax', syntax.replace('monokai', 'default'));
+    else if( localStorage.getItem('bg') === 'rgb(6, 23, 37)') {
+        localStorage.setItem('bg', 'rgb(255, 255, 255)');
+        localStorage.setItem('cc', '#333');
+        localStorage.setItem('syntax', syntax.replace('monokai', 'default'));
     }
 
-    document.body.style.backgroundColor = sessionStorage.getItem('bg');
-    document.body.style.color = sessionStorage.getItem('cc');
-    document.getElementById('syntax').setAttribute('href', sessionStorage.getItem('syntax'));
+    document.body.style.backgroundColor = localStorage.getItem('bg');
+    document.body.style.color = localStorage.getItem('cc');
+    document.getElementById('syntax').setAttribute('href', localStorage.getItem('syntax'));
 }
